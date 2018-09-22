a28b2de64edf8d04a1e9cf8737e46ea7f42813e2
Unknown <sn-z777@outlook.com>
2018-09-22 18:13:06 +0200

    node class

--- a-star-pathfinding/src/astar/Node.java
+++ a-star-pathfinding/src/astar/Node.java
@@ -1,5 +1,43 @@
 package astar;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Node {
+	// Coordinates
+	private int x, y;
+	// Parent node to reconstruct the path
+	private Node parent;
+	private boolean start;
+	private boolean end;
+	private List<Node> neighbors = new ArrayList<>();
+
+	public Node(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {return x;}
+
+	public void setX(int x) {this.x = x;}
+
+	public int getY() {return y;}
+
+	public void setY(int y) {this.y = y;}
+
+	public void addNeighbor(Node neighbor) {neighbors.add(neighbor);}
+
+	public List<Node> getNeighbors() {return this.neighbors;}
+
+	public boolean isEnd() {return end;}
+
+	public void setEnd(boolean end) {this.end = end;}
+
+	public boolean isStart() {return start;}
+
+	public void setStart(boolean start) {this.start = start;}
+
+	public Node getParent() {return parent;}
+
+	public void setParent(Node parent) { this.parent = parent; }
 }
